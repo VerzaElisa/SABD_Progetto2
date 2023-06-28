c15e215f007d1f9f5460f3d06bbe60465f034899
VerzaElisa <elisaverza.gm@gmail.com>
2023-06-28 16:00:07 +0200

    query2 in corso

--- FlinkScripts/query2.py
+++ FlinkScripts/query2.py
@@ -5,6 +5,9 @@ from FirstElementTimestampAssigner import FirstElementTimestampAssigner
 
 from pyflink.common import SimpleStringSchema,WatermarkStrategy,Time ,Duration
 from pyflink.datastream import StreamExecutionEnvironment
+from pyflink.table import StreamTableEnvironment, Schema
+from pyflink.table.expressions import col, UNBOUNDED_RANGE, CURRENT_RANGE
+from pyflink.table.window import Over
 from pyflink.datastream.connectors import KafkaSource
 from pyflink.datastream.window import TumblingEventTimeWindows
 from pyflink.datastream import StreamExecutionEnvironment
@@ -12,7 +15,7 @@ from pyflink.common.typeinfo import Types
 from pyflink.datastream import StreamExecutionEnvironment
 from pyflink.datastream.window import TimeWindow
 from pyflink.datastream.connectors import StreamingFileSink
-from pyflink.datastream.functions import ReduceFunction, ProcessAllWindowFunction
+from pyflink.datastream.functions import ReduceFunction, WindowFunction
 
 format = "%d-%m-%Y|%H:%M:%S.%f"
 
@@ -22,6 +25,8 @@ def csvToList(f):
 
 def kafkaread():
     env = StreamExecutionEnvironment.get_execution_environment()
+    t_env = StreamTableEnvironment.create(env)
+
     env.set_parallelism(1)
     env.add_jars("file:///opt/flink-apps/flink-sql-connector-kafka-1.17.1.jar")
     
@@ -36,13 +41,31 @@ def kafkaread():
     
     ds=env.from_source(source, WatermarkStrategy.for_monotonous_timestamps(), "Kafka Source")\
           .map(func=csvToList)\
-          .key_by(key_selector=lambda f:f[0])
-    ds = ds.window(TumblingEventTimeWindows.of(Time.minutes(30)))
-    #Alternativa a queste due righe, apply dove ogni riga diventa una tupla con nome-data-ora, key by su questo campo, max e min su questo campo
+          .key_by(key_selector=lambda f:f[0])\
+          .window(TumblingEventTimeWindows.of(Time.minutes(30)))
+    
+    #Alternativa a queste due righe, apply dove ogni riga diventa una tupla con nome data-ora, key by su questo campo, max e min su questo campo
     ds_min = ds.reduce(ReduceFunctionMin()).map(lambda f: (f[0], f[2]))
     ds_max = ds.reduce(ReduceFunctionMax()).map(lambda f: (f[0], f[2]))
-    ds_min.union(ds_max).key_by(lambda x: x[0]).reduce(lambda a, b: (a[0], float(a[1]) - float(b[1]))).print()
+    ds_all = ds_min.union(ds_max)\
+                   .key_by(lambda x: x[0])\
+                   .reduce(lambda a, b: (a[0], float(a[1]) - float(b[1])))\
+                   .map(lambda f: (str(f[0]), float(f[1])), Types.TUPLE([Types.STRING(), Types.FLOAT()]))
+    
+    # interpret the insert-only DataStream as a Table
+    t = t_env.from_data_stream(ds_all)
+    t.print_schema()
+    t_env.create_temporary_view("InputTable", t)
+    res_table = t_env.sql_query("SELECT f0 f1 FROM (SELECT f0 f1, ROW_NUMBER() OVER (PARTITION BY f0 f1 ORDER BY f1 asc) AS rownum FROM InputTable)")
+
+    # interpret the insert-only Table as a DataStream again
+    res_ds = t_env.to_data_stream(res_table)
+
+    # add a printing sink and execute in DataStream API
+    res_ds.print()
+
     env.execute('kafkaread')
+
 
 class ReduceFunctionMin(ReduceFunction):
     def reduce(self, a, b):
